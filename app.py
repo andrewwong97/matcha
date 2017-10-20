cf5bdf9e81b507cf05ac7ff0f601d9a7aac851fb
Andrew Wong <aznhippos@gmail.com>
2017-10-20 16:36:23 -0400

    Add basic authentication with mongo

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, request, redirect, url_for
 from flask_pymongo import PyMongo
+import bcrypt
 
 import random
 
@@ -12,11 +13,39 @@ app.config['MONGO_URI'] = 'mongodb://oose:letmein@ds015962.mlab.com:15962/matcha
 mongo = PyMongo(app)
 
 @app.route('/')
-def hello_world():
-  return render_template('index.html')
+def index():
+	if 'email' in session:  # user email identifier
+		return 'You are logged in as {}'.format(session['email'])
+
+	return render_template('index.html')
 
 
-@app.route('/listings/display')
+# Basic Login
+@app.route('/login')
+def login():
+	return ''
+
+
+# Basic Register
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+	if request.method == 'POST':
+		users = mongo.db.users
+		u = users.find_one({'email': request.form['email']})
+
+		if u is None:  # no user exists for this email
+			hashpass = bcrypt.hashpw(request.form['password'].encode('utf8'), bcrypt.gensalt())
+			users.insert({'email': request.form['email'], 'password': hashpass})
+			session['email'] = request.form['email']
+			return redirect(url_for('index'))
+
+		return 'Email is already being used for another account'
+
+	return render_template('/index.html')
+
+
+
+@app.route('/listings/display', methods=['GET'])
 def show_job_listings():
 	listings = mongo.db.listings.find({})  # all listings
 
