8cec19449716a0d5853b73ca0eec04a521145d12
Andrew Wong <aznhippos@gmail.com>
2017-10-22 13:42:16 -0400

    Fix react router only changing the url and not rendering components

--- js/routes.js
+++ js/routes.js
@@ -1,41 +1,43 @@
 import React from 'react';
 import {
   BrowserRouter as Router,
+  browserHistory,
   Route,
   Link,
   withRouter,
   Switch
 } from 'react-router-dom';
 
-
-import Home from './Home';
-import Listings from './Listings';
-import Login from './Login';
-import Signup from './Signup';
-
-const LoginWithRouter = withRouter(Login);
-const ListingsWithRouter = withRouter(Listings);
+import Home from './components/Home';
+import Listings from './components/Listings';
+import Login from './components/Login';
+import Signup from './components/Signup';
 
 export default class AppRouter extends React.Component {
+  handle() {
+    console.log(this.props.location);
+  }
+
   render() {
     return (
       <div className="AppRouter">
-        <Router>
+        <Router history={browserHistory}>
           <div>
             <div className="Nav-wrapper">
-            <div className="logo">
-              <h1>Matcha</h1>
+              <div className="logo">
+                <h1>Matcha</h1>
+              </div>
+              <ul className="Nav" onClick={this.handle.bind(this)}>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/login">Login</Link></li>
+                <li><Link to="/listings">Listings</Link></li>
+              </ul>
             </div>
-            <ul className="Nav">
-              <li><Link to="/">Home</Link></li>
-              <li><Link to="/login">Login</Link></li>
-              <li><Link to="/listings/display">Listings</Link></li>
-            </ul>
-          </div>
-          
+            <Switch>
             <Route exact path="/" component={Home} />
-            <Route exact path="/login" component={LoginWithRouter} />
-            <Route exact path="/listings/display" component={ListingsWithRouter} />
+            <Route path="/login" component={Login} />
+            <Route path="/listings" component={Listings} />
+            </Switch>
           </div>
         </Router>
       </div>
