26358e00247d90f83135a799a26c106b8ed07b23
Andrew Wong <aznhippos@gmail.com>
2017-11-17 20:38:46 -0500

    Avoid circular dependency by importing everything from views

--- matcha/tests.py
+++ matcha/tests.py
@@ -1,8 +1,7 @@
 from nose.tools import assert_equals, assert_raises
-from util import *
-
-def test_empty_student():
-	s = Student()
-	assert_equals(1, 1)
+from views import *
 
 
+def test_create_empty_student():
+    s = Student()
+    assert isinstance(s, Student)
