7aed36293af094ceb2d3d749cf08123fb4137fb7
Tucker Chapin <tuckerchapin@tuckerbook-air.local>
2017-11-17 21:19:06 -0500

    Extracted EmployerProfile into it's own component to follow with other style.

--- react/pages/employer-profile.js
+++ react/pages/employer-profile.js
@@ -1,53 +1,9 @@
-import React from 'react';
-import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
-import Layout from '../components/layout';
-import withAuth from '../util/withAuth';
+import Layout from '../components/layout'
 
-const baseUrl = require('../vars.json').baseUrl;
+import EmployerProfile from '../components/EmployerProfile';
 
-class EmployerProfile extends React.Component {
-    constructor(props){
-        super(props);
-
-        this.state = {
-            matches: [{id: 1, name: 'Software Engineer', student_name: 'Andrew Wong'}],
-            username: this.props.url.query.profile_id,
-            profile: null,
-        };
-    }
-
-    componentDidMount() {
-        fetch(`${baseUrl}/v1/getProfile/${this.state.username}`, {method: 'GET'})
-            .then((res) => res.json())
-            .then((user) => this.setState({ user }));
-    }
-
-    renderUserDetails() {
-        return this.state.profile ?
-            <div className="company-details">
-                <h1 className="name">
-                    {this.state.profile.company_name}
-                    <span className="thin"> ({this.state.username})</span>
-                </h1>
-            </div> : '';
-    }
-
-    render() {
-        return (
-            <Layout title="Employer Profile">
-                <div className="EmployerProfile">
-                    <div className="logo-placeholder"></div>
-                    {this.renderUserDetails()}
-
-                    <BootstrapTable data={ this.state.matches }>
-                        <TableHeaderColumn dataField='id' isKey={ true }>ID</TableHeaderColumn>
-                        <TableHeaderColumn dataField='name'>Job Title</TableHeaderColumn>
-                        <TableHeaderColumn dataField='student_name'>Student Name</TableHeaderColumn>
-                    </BootstrapTable>
-                </div>
-            </Layout>
-        )
-    }
-}
-
-export default withAuth(EmployerProfile);
+export default ({ url }) => (
+    <Layout title="Employer Profile">
+        <EmployerProfile username={url.query.profile_id} />
+    </Layout>
+)
