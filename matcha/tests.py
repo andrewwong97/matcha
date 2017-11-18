ebf481f2f182b1b83eb8961cdfcc52a76ec11d86
Andrew Wong <aznhippos@gmail.com>
2017-11-17 20:48:06 -0500

    Test templates

--- matcha/tests.py
+++ matcha/tests.py
@@ -1,7 +1,29 @@
-from nose.tools import assert_equals, assert_raises
+from nose.tools import *
 from views import *
 
 
 def test_create_empty_student():
     s = Student()
     assert isinstance(s, Student)
+
+
+@raises(AttributeError)
+def test_empty_student_no_attributes():
+    s = Student()
+    username = s.username
+
+
+@raises(AttributeError)
+def test_util_empty_student_to_dict():
+    s = Student()
+    student_to_dict(s)
+
+
+@raises(KeyError)
+def test_util_incomplete_dict_to_student():
+	d = {
+		'username': '<username>',
+		'password': '<password>',
+		'skills': []
+	}
+	dict_to_student(d)
