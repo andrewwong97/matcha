1c177515b1a75f35f6512770292974946c2aab75
Andrew Wong <aznhippos@gmail.com>
2017-12-10 03:07:53 -0500

    Db populate code

---
+++ matcha/tests/populate_listings.py
@@ -0,0 +1,27 @@
+# Use only if database has been flushed for whatever reason
+
+import pymongo, json
+
+def populate():
+	client = pymongo.MongoClient('mongodb://matcha:letmein@ds129146.mlab.com:29146/matcha2')
+
+	db = client.matcha2
+
+	listings = json.loads(open('fixtures.json','r').read())
+
+
+	for l in listings:
+		db.Listing.insert({
+			'title': l['name'],
+			'employer': l['employer'],
+			'salary': l['salary'],
+			'desired_skills': list(l['desired_skills']),
+			'job_type': list(l['job_type'])
+			})
+
+if __name__ == '__main__':
+	ans = raw_input("DANGER! This may create duplicates of objects already in the database. Are you sure you want to proceed? (y/n)")
+	if ans.lower() == 'y':
+		populate()
+	else:
+		print 'Exiting...'
