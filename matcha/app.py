4dd117c322c97943553eacc16bef884b68fdb6be
Andrew Wong <wongandrew97@gmail.com>
2017-12-16 04:11:07 -0500

    Set up docker compose and app config with mongo image

--- matcha/app.py
+++ matcha/app.py
@@ -15,3 +15,4 @@ if __name__ == '__main__':
     nltk.download('punkt')
     nltk.download('stopwords')
     app.run(host='0.0.0.0')
+
--- matcha/config.py
+++ matcha/config.py
@@ -1,5 +1,11 @@
+import os
+
 MONGOALCHEMY_DATABASE = 'matcha2'
-MONGOALCHEMY_CONNECTION_STRING = 'mongodb://matcha:letmein@ds129146.mlab.com:29146/matcha2'
+# MONGOALCHEMY_CONNECTION_STRING = 'mongodb://matcha:letmein@ds129146.mlab.com:29146/matcha2'
+MONGOALCHEMY_USER = 'matcha'
+MONGOALCHEMY_PASSWORD = 'letmein'
+MONGOALCHEMY_HOSTNAME = 'matcha_mongo'
+MONGOALCHEMY_CONNECTION_STRING = 'mongodb://matcha_mongo:27017'
 SECRET_KEY = 'matcha'
 DEBUG = True
 HOST = '0.0.0.0'
